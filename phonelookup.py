7e469ffdd9aa17eb19a3c46aaabaf03b6015ae65
OlayinkaOP <OlayinkaA.op.ac.nz>
2022-10-09 21:37:12 +1300

    phonebook lookup

---
+++ phonelookup.py
@@ -0,0 +1,13 @@
+def phonebook_lookup(filename):
+	phonebook_dict = dict()
+	unwanted_chars = ".,- "
+  	with open(filename,'r') as f:
+   		 for line in f:
+                raw_words = line.split()  
+                phonebook_dict[raw_words[0].strip(unwanted_chars)] = raw_words[1].strip(unwanted_chars)
+  	return(phonebook_dict)
+
+print("\nPrinting your phonebook dict as key-value pair of name-phone")
+print(phonebook_lookup("phonebook.txt"))
+print("\nPrinting the length of your phonebook dict ")
+print(len(phonebook_lookup("phonebook.txt")))
